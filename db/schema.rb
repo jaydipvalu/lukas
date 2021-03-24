4e9d4f796915de5ad9fbcc1801a41247e5c0f83b
jaydipvalu <jaydipvalu88@gmail.com>
2021-03-24 11:19:11 +0530

    migration drop user

--- db/schema.rb
+++ db/schema.rb
@@ -124,18 +124,6 @@ ActiveRecord::Schema.define(version: 2021_03_19_124757) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   create_table "versions", force: :cascade do |t|
     t.string "name", limit: 50
     t.integer "model_id"
